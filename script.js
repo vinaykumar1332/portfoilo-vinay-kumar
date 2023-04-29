ac652597771e3cf2f481bf232372bb6a98dc15be
vinaykumar1332 <84768570+vinaykumar1332@users.noreply.github.com>
2023-04-29 11:31:51 +0000

    Main

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+//----------SCROLL-UP-BUTTON--------------
+  let mybutton = document.getElementById("myBtn");
+    
+  // When the user scrolls down 50px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
   
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+ 
