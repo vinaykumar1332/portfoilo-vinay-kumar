90a1b4fad0ca3ac443f8086a5266b4a86ef74f14
vinaykumar1332 <84768570+vinaykumar1332@users.noreply.github.com>
2023-05-26 23:50:59 +0530

    Update script.js
    code change

--- script.js
+++ script.js
@@ -15,7 +15,20 @@
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
     document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollTop = 20;
   }
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+/*----loader---*/
+
 
  
